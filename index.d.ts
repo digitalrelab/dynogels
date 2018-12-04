e9e36969365ded8fa2320df811acf4f888c3e650
stephentuso <tusodev@gmail.com>
2018-12-04 13:58:52 -0500

    Add validate to type definitions

--- index.d.ts
+++ index.d.ts
@@ -89,6 +89,7 @@ export interface Model {
     after(action: LifeCycleAction, listner: (item: Item) => void): void;
     before(action: LifeCycleAction, listner: (data: any, next: (err: Error | null, data: any) => void) => void): void;
     config(config: ModelConfig): { name: string };
+    validate(item: any): { error: Error  };
 }
 
 export type DynogelsItemCallback = (err: Error, data: Item) => void;
