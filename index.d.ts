08acd8fd999d1370f711dec08dabefaf845aaf28
stephentuso <tusodev@gmail.com>
2019-11-05 12:12:07 -0500

    fix: update LastEvaluatedKey type

--- index.d.ts
+++ index.d.ts
@@ -194,7 +194,7 @@ export interface QueryResponse<T = Record<any, any>, K extends keyof T = keyof T
   Items: Array<Item<T, K>>,
   Count: number,
   ScannedCount: number,
-  LastEvaluatedKey?: Partial<T> | string | number,
+  LastEvaluatedKey?: Partial<T>,
 }
 
 // Dynogels Query
